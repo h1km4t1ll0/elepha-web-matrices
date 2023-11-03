fa5c6099df4adfff0fef2199f4be5894a5e6baa0
Calorific <kknd3390@mail.ru>
2023-11-04 01:45:02 +0700

    feat: инициализация проекта

---
+++ src/main.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './pages/HomePage.tsx'
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+)
---
+++ src/pages/HomePage.tsx
@@ -0,0 +1,13 @@
+import { FC } from 'react'
+import { Typography } from 'antd'
+
+const App: FC = () => {
+
+  return (
+    <>
+      <Typography.Title>Elepha Health</Typography.Title>
+    </>
+  )
+}
+
+export default App
