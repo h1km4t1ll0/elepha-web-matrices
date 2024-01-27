0e061483c09bc17161a45772e0897b4cb3e345c6
hikmatillo <dolgovdaniil007@gmail.com>
2024-01-28 00:23:58 +0700

    fix: css

--- src/pages/MatrixPage/MatrixPage.tsx
+++ src/pages/MatrixPage/MatrixPage.tsx
@@ -1,6 +1,6 @@
 import React, { FC, useEffect, useState } from 'react'
 import { Navigate, useParams } from 'react-router-dom'
-import { Space, Typography } from 'antd'
+import {Card, Space, Typography} from 'antd'
 import { Matrix as MatrixType } from '@/shared/api/types.js'
 import { Matrix } from '@/widgets/Matrix/matrix.js'
 import MatrixApi from '@/shared/api/api.js'
@@ -22,39 +22,47 @@ export const MatrixPage: FC = () => {
   if (!name || !['rest', 'balance', 'activation'].includes(name))
     return <Navigate to='/matrix/rest' />
 
-  return <>
-    <Space direction="vertical" align="center" style={{ width: '100%', marginTop: '100px', marginBottom: '20px'}}>
-      <div style={{ position: 'relative', alignItems: 'center' }}>
-        <div style={{ display: 'flex', justifyContent: 'center' }}>
-          <div style={{ maxWidth: '310px' }}>
-            {/*<img src={Logo} alt='Elepha health' />*/}
-            <Typography.Title level={1}>CureSound</Typography.Title>
+  return (
+    <div style={{
+      position: 'absolute',
+      top: '50%',
+      left: '50%',
+      transform: 'translate(-50%, -50%)',
+    }}>
+      <Space direction="vertical" align="center" style={{
+        height: '100%',
+        width: '100%',
+        marginTop: '100px',
+        marginBottom: '20px'}}>
+        <div style={{ position: 'relative', alignItems: 'center' }}>
+          <div style={{ display: 'flex', justifyContent: 'center' }}>
+            <div style={{ maxWidth: '310px' }}>
+              {/*<img src={Logo} alt='Elepha health' />*/}
+              <Typography.Title level={1}>CureSound</Typography.Title>
+            </div>
           </div>
-        </div>
-        <Space direction="vertical" align="center" style={{}}>
-          <Matrix matrix={matrix} />
-          <Space direction="vertical" align="center" style={{
-            textAlign: 'center',
-            marginRight: '40px',
-            marginLeft: '40px'
-          }}>
-            <Typography.Text
-                style={{ fontSize: '23px'}}>
-              Раскройте потенциал ресурсного состояния
-            </Typography.Text>
-            <Typography.Link
-              style={
-                {
-                  fontSize: '20px',
-                  justifyContent: 'center',
-                }
-              }
-              href="https://www.elephahealth.com/">
-              Сервис wellness - услуг с выездом в отель или на дом
-            </Typography.Link>
+          <Space direction="vertical" align="center" style={{verticalAlign: 'middle'}}>
+            <Matrix matrix={matrix} />
+            <Card className='matrixCard' style={{minWidth: '300px'}}>
+              <Space direction="vertical" align="center" style={{textAlign: 'center'}}>
+                <Typography.Text
+                    style={{ fontSize: '23px'}}>
+                  Раскройте потенциал ресурсного состояния
+                </Typography.Text>
+                <Typography.Link
+                  style={
+                    {
+                      fontSize: '20px',
+                      justifyContent: 'center',
+                    }
+                  }
+                  href="https://www.elephahealth.com/">
+                  Сервис wellness - услуг с выездом в отель или на дом
+                </Typography.Link>
+              </Space>
+              </Card>
           </Space>
-        </Space>
-      </div>
-    </Space>
-  </>
+        </div>
+      </Space>
+  </div>)
 }
