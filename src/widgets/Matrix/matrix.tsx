748163d84156fbe780b71d33c2be419708d64b3e
hikmatillo <dolgovdaniil007@gmail.com>
2024-01-27 23:25:24 +0700

    fix: tsc

--- src/widgets/Matrix/matrix.tsx
+++ src/widgets/Matrix/matrix.tsx
@@ -3,7 +3,6 @@ import { MatrixProps } from '@/widgets/Matrix/types.js'
 import { Card, Skeleton } from 'antd'
 import { AudioPlayer } from '@/shared/audioPlayer/audioPlayer.js'
 import { MatrixInfo } from '@/entities/MatrixInfo/matrixInfo.js'
-import { Typography } from 'antd'
 
 export const Matrix: FC<MatrixProps> = ({ matrix }) => {
   if (!matrix)
